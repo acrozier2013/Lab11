79db24b948a3c8d820910a487a4f711f6cbfaada
acrozier2013 <43171739+acrozier2013@users.noreply.github.com>
2018-12-02 10:57:48 -0500

    Add files via upload

---
+++ MyMath.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab09
+{
+    class MyMath
+    {
+        //public double operand1;
+        //public double operand2;
+
+        //public double result;
+
+        //public void GetResult(double result, double operand1, double operand2)
+        //{
+        //    this.result = operand1 * operand2;    
+        //}
+
+
+        public double operand1;
+        public double operand2;
+        public int year;
+        public string color;
+
+        private int speed;
+        public int result;
+        
+
+        public void setResult(int setResult)
+        {
+            this.result = setResult;
+        }
+
+        public int getResult()
+        {
+            return result;
+        }
+
+        public void Honk()
+        {
+            Console.WriteLine("Honk Honk");
+        }
+
+        public void Beep()
+        {
+            Console.WriteLine("Beep Beep");
+        }
+
+
+        //public void speedUp()
+        //{
+        //    if (speed < topSpeed)
+        //    {
+        //        speed++;
+        //    }
+        //    Console.WriteLine("Speeding Up....");
+        //}
+
+        public void slowDown()
+        {
+            if (speed > 0)
+            {
+                speed--;
+            }
+            Console.WriteLine("Slowing Down....");
+        }
+
+        public void ToString()
+        {
+            Console.WriteLine("I'm a " + operand1 + " " + operand2 + " and I'm going " + speed + "mph");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,68 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CrozierLabWk11
+namespace Lab09
 {
     class Program
     {
-        static void Main(string[] args)
+        static void GetResult(string[] args)
+        //{
+        //    MyMath mathproblem1 = new MyMath();
+        //    MyMath mathproblem2 = new MyMath();
+
+        //    mathproblem1.operand1 = 2;
+        //    mathproblem1.operand2 = 2;
+
+        //    mathproblem1.result = 0;
+        //    mathproblem1.GetResult();
+
+        //    Console.WriteLine(mathproblem1.GetResult());
+
+
         {
-            //string[] names = {"Al Dente", "Anna Graham", "Earle Bird", "Ginger Rayle", "Iona Ford"};
+            MyMath myFirstCar = new MyMath();
+            MyMath mySecondCar = new MyMath();
+            MyMath myThirdCar = new MyMath();
 
-            //Console.WriteLine(names[1]);
-            //Console.WriteLine(names[names.Length - 1]);
-
-
-            //string[] months = new string[12];
-
-            //months[0] = "January";
-            //months[1] = "February";
-            //months[2] = "March";
-            //months[3] = "April";
-            //months[4] = "May";
-            //months[5] = "June";
-            //months[6] = "July";
-            //months[7] = "August";
-            //months[8] = "September";
-            //months[9] = "October";
-            //months[10] = "November";
-            //months[11] = "December";
-
-            //for (int i = 0; i < months.Length; i++)
+            myFirstCar.operand1 = 2;
+            myFirstCar.operand2 = 2;
+            myFirstCar.result = 280;
+            myFirstCar.setResult(280);
+                        //myFirstCar.speedUp();
+                        //myFirstCar.speedUp();
+            Console.WriteLine(myFirstCar.getResult());
+            //for (int i = 0; i <= 200; i++)
             //{
-            //    Console.WriteLine((i) + ": " + months[i]);
+            //    myFirstCar.speedUp();
             //}
+            myFirstCar.ToString();
 
-
-            //string[] seasons = {"Spring", "Summer", "Fall", "Winter"};
-
-            //foreach (string season in seasons)
-            //{
-            //    Console.WriteLine(season);
-            //}
-
-            int[] randomNum = new int[1000];
-            Random random = new Random();
-
-            for (int i = 0; i < randomNum.Length; i++)
-            {
-                randomNum[i] = random.Next(0, 1000);
-            }
-
-            foreach (int a in randomNum)
-            {
-                Console.WriteLine(a.ToString());
-            }
+           
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
